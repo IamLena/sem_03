cdf8ed8595d05d6e08849c8dc712a91dcac1a351
led17u819 <helly.luchina@gmail.com>
2018-11-25 17:10:26 +0300

    make and app.c

--- lr_03/app.c
+++ lr_03/app.c
@@ -10,33 +10,37 @@ int main(void)
     double **matrix;
     int n, m;
     double *vector;
-//    double *result;
-//    unsigned long t1, t2;
+    double *result;
+    unsigned long t1, t2;
 
     rc = get_matrix(&matrix, &n, &m);
     if (rc == OK)
     {
-        //print_matrix(matrix, n, m);
-        printf("\n----SPARSE----\n");
+        if (n < 10 && m < 10)
+            print_matrix(matrix, n, m);
         rc = get_vector(&vector, &m);
         if (rc == OK)
         {
-//            print_array(vector, m);
-//            t1 = tick();
-//            multiply_usual(matrix, vector, n, m, &result);
-//            t2 = tick();
-//            printf("time - %lu\n", t2 - t1);
+            if (m < 10)
+                print_array(vector, m);
 
-            printf("\n----SPARSE----\n");
+            t1 = tick();
+            multiply_usual(matrix, vector, n, m, &result);
+            t2 = tick();
+            printf("usual multiply time - %lu\n", t2 - t1);
+            print_array(vector, m);
             s_matrix a;
             s_vector b;
+            print_matrix(matrix, n, m);
+            print_array(vector, m);
+            //s_vector res;
             make_sparse_vector(vector, m, &b);
             make_sparse_matrix(matrix, n, m, &a);
-            mult_spare_arrays(b, b);
-            //s_vector result = mult_sparse(a, b);
-            printf("\nMULT\n");
+            printf("mult\n");
+            t1 = tick();
             mult_sparse(a, b);
-
+            t2 = tick();
+            printf("sparse multiply time - %lu\n", t2 - t1);
         }
     }
     if (rc == IO_ERR)
